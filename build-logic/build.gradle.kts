351c8a2dbc8c38bf6a3e2ccf3236c58aae36ec18
SDenTronS <ddtronin@gmail.com>
2026-02-28 17:24:05 +0300

    fix: create Kotlin plugins in build-logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,3 +10,24 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:${libs.versions.springBoot.get()}")
 }
+
+gradlePlugin {
+    plugins {
+        create("conventionsJava") {
+            id = "conventions-java"
+            implementationClass = "ConventionsJavaPlugin"
+        }
+        create("conventionsJavaLibrary") {
+            id = "conventions-java-library"
+            implementationClass = "ConventionsJavaLibraryPlugin"
+        }
+        create("conventionsLombok") {
+            id = "conventions-lombok"
+            implementationClass = "ConventionsLombokPlugin"
+        }
+        create("conventionsSpringApp") {
+            id = "conventions-spring-app"
+            implementationClass = "ConventionsSpringAppPlugin"
+        }
+    }
+}
